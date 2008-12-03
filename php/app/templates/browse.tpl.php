bcda578ed71d1e67416ee7d6ad699a0262c99379
Ian Davis <nospam@iandavis.com>
2008-12-03 10:01:15 +0000

    Added visualisation of vocab relationships

--- php/app/templates/browse.tpl.php
+++ php/app/templates/browse.tpl.php
@@ -1,29 +1,13 @@
 <?php
 if (isset($terms)) {
-  echo '<h1>Browse OpenVocab</h1>';
+  echo '<h1>Browse Relations</h1>';
+  
 }
-/*
-
-    <div id="gearspace" style="float:left;">
-      <strong>You need to upgrade your Flash Player</strong>
-    </div>
-    <div style="clear:both;">
-   
-    <script type="text/javascript" src="/js/graph-gear/swfobject/swfobject.js"></script>
-
-    <script type="text/javascript">
-      // <![CDATA[
-      var so = new SWFObject("/swf/GraphGear.swf", "graphgear", "725", "600", "8");
-      so.addVariable("graphXMLFile", "/browse/graph"); // rename to your xml file
-      so.addParam("scale", "noborder");
-      so.addParam("salign", "tl");
-              
-      so.write("gearspace");
-    
-      // ]]>
-    </script>
-*/
 
 
 
 ?>
+<div class="img">
+  <img src="/browse/relations/img" width="100%" />
+</div>
+
