f190e817d3895366e904f1f8b4e6dcf9c83a92aa
Ian Davis <nospam@iandavis.com>
2008-12-03 10:04:42 +0000

    Added visualisation of vocab relationships

---
+++ php/app/templates/browserelations.tpl.php
@@ -0,0 +1,6 @@
+<h1>Browse Relations</h1>
+<p>This diagram shows properties with domains and ranges analagous to an entity relationship diagram. Each node represents a class. An arc is added for each OpenVocab property connecting the node that represents the domain of the property to the node that represents its range.</p>
+<div class="img">
+  <img src="/browse/relations/img" width="100%" />
+</div>
+
